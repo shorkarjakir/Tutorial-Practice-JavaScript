3d4267ac93413355d8be7c03c70ab7fac3cedcda
jakir <shorkarjakir@gmail.com>
2023-07-30 23:18:07 +0600

    last one added

---
+++ summary.js
@@ -0,0 +1,33 @@
+var destinations = ['sajek', 'coxs bazar', 'thaild', 'singapore'];
+
+destinations[3] = 'japan';
+
+console.log(destinations[2]);
+console.log(destinations[3]);
+
+var index = destinations.indexOf('thaild');
+console.log(index);
+
+destinations.push('rossia');
+destinations.pop();
+
+console.log(destinations);
+
+// conditionals 
+<, >, =, !=, <=, >=,
+
+// Full field all the condition &&
+// Just full field one of them ||
+
+if (destinations[1] === 'sajek') {
+  
+}
+else if (destinations[2] === 'bali') {
+  
+}
+else if (destinations.length !== 3) {
+  
+}
+else {
+  
+}
