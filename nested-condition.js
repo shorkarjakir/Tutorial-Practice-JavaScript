42a20a13bf156c9c18369c19a25f077425534339
jakir <shorkarjakir@gmail.com>
2023-07-30 22:37:18 +0600

    hard to memorize

---
+++ nested-condition.js
@@ -0,0 +1,18 @@
+var math = true;
+var geometry = true;
+var straightline = true;
+
+
+if (math == true) {
+  if (geometry == true) {
+    if (straightline == true) {
+      
+    }
+    else {
+      console.log('tomake shikte hobe')
+    }
+  }
+  else {
+    console.log('GOOD Enough at all')
+  }
+}
