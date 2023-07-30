ffab5ecfeb2047806256306115218b58a5e48722
jakir <shorkarjakir@gmail.com>
2023-07-30 22:26:45 +0600

    multi stage condition added

---
+++ multi-stage-conditional.js
@@ -0,0 +1,17 @@
+var money = 40;
+var toast = 45;
+var cake = 35;
+var Bread = 20;
+
+if (money > toast) {
+  console.log('Tahole khabo')
+}
+else if (money > cake) {
+  console.log('Tahole cake khabo')
+}
+else if (money > Bread) {
+  console.log('Tahole bread khabo')
+}
+else {
+  console.log('kichiu khabo na')
+}
